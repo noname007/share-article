e7bfc564a3f77f217be26d074878653046c1685c
y.zz <yz0437@gmail.com>
2015-08-14 21:11:02 +0800

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.1.115;dbname=share',
+    'username' => 'root',
+    'password' => '123a123a',
+    'charset' => 'utf8',
+];
